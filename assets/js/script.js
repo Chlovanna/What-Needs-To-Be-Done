6ea94dbf456756b6e249df38df88e722de208b43
Chlovanna <ashdmt39@gmail.com>
2022-04-21 12:12:14 -0400

    updated codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,41 +11,30 @@ $(document).ready(function () {
     localStorage.setItem(time, text);
   });
 
-  // to load LocalStorage data for eighteen hours (workhours) 24 hr - 12 hr conversion.
-  $("#hour6 .description").val(localStorage.getItem("hour6"));
-  $("#hour7 .description").val(localStorage.getItem("hour7"));
-  $("#hour8 .description").val(localStorage.getItem("hour8"));
-  $("#hour9 .description").val(localStorage.getItem("hour9"));
-  $("#hour10 .description").val(localStorage.getItem("hour10"));
-  $("#hour11 .description").val(localStorage.getItem("hour11"));
-  $("#hour12 .description").val(localStorage.getItem("hour12"));
-  $("#hour13 .description").val(localStorage.getItem("hour13"));
-  $("#hour14 .description").val(localStorage.getItem("hour14"));
-  $("#hour15 .description").val(localStorage.getItem("hour15"));
-  $("#hour16 .description").val(localStorage.getItem("hour16"));
-  $("#hour17 .description").val(localStorage.getItem("hour17"));
-  $("#hour18 .description").val(localStorage.getItem("hour18"));
+  // save tasks to local storage
+  for (let i = 9; i < 18; i++) {
+    var saveTasks = localStorage.getItem(i);
+    var timeInfo = $(`[id=${i}]`).children(".description");
+    timeInfo.val(saveTasks);
+  }
 
   function timeTracker() {
     //declare currenHour.
-    var currentHour = moment().hour(); 
+    var currentHour = moment().hour();
 
-    // toloop over time blocks
+    // to loop over time blocks
     $(".time-block").each(function () {
       var workHour = parseInt($(this).attr("id").split("hour")[1]);
-      console.log(workHour, currentHour);
+
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
 
       if (workHour < currentHour) {
         $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
       } else if (workHour === currentHour) {
-        $(this).removeClass("past");
         $(this).addClass("present");
-        $(this).removeClass("future");
       } else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
         $(this).addClass("future");
       }
     });
