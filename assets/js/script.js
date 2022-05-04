1bf57f7925c392979fdbc6a07cea70d74ea59de3
Chlovanna <ashdmt39@gmail.com>
2022-05-04 11:32:08 -0400

    update get.item from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,18 @@ $(document).ready(function () {
     localStorage.setItem(time, text);
   });
 
+  // get items from local storage
+  // $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  
   // save tasks to local storage
   for (let i = 9; i < 18; i++) {
     var saveTasks = localStorage.getItem(i);
@@ -42,16 +54,3 @@ $(document).ready(function () {
   timeTracker(); //call timeTracker
 });
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
